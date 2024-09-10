7d235819252f005ea9980310063757530286d07a
RfFad <ashiap135@gmail.com>
2024-09-10 16:33:19 +0700

    tanpa status

--- main.js
+++ main.js
@@ -4,26 +4,18 @@ function cekhodam(e) {
     if (nama === '') {
         alert('nama tidak boleh kosong');
     } else {
-        let status = ['isi', 'kosong'];
-        const randomStatus = Math.floor(Math.random() * status.length);
         const result = document.querySelector('#result');
         const image = document.querySelector('#gmbr-hitam');
-        
-        if (status[randomStatus] === 'isi') {
-            let hitam = ['mas rusdi', 'psht', 'si imut', 'farhan kebab', 'mas amba', 'Ilham Penyu', 'si geram', 'rehanslebew'];
-            let imageHitam = ['masrusdi.webp', 'psht.jpg', 'siimut.jpg', 'farhankebab.webp', 'masamba.jpg', 'ilhampenyu.jpg', 'geramnyaaku.jpg', 'rehanslebew'];
-            const hitamStatus = Math.floor(Math.random() * hitam.length);
 
-            result.innerHTML = `${nama}, khodam hitam kamu (${hitam[hitamStatus]})`;
-            image.src = imageHitam[hitamStatus]; 
-            const search = document.querySelector('#search');
-            search.value = '';
-        } else {
-            result.innerHTML = `${nama}, khodam hitam kamu kosong`;
-            image.style.display = 'none'; // Hide image if khodam is empty
-            const search = document.querySelector('#search');
-            search.value = '';
-        }
+        let hitam = ['mas rusdi', 'psht', 'si imut', 'farhan kebab', 'mas amba', 'Ilham Penyu', 'si geram', 'rehanslebew'];
+        let imageHitam = ['masrusdi.webp', 'psht.jpg', 'siimut.jpg', 'farhankebab.webp', 'masamba.jpg', 'ilhampenyu.jpg', 'geramnyaaku.jpg', 'rehanslebew'];
+        const hitamStatus = Math.floor(Math.random() * hitam.length);
+
+        result.innerHTML = `${nama}, khodam hitam kamu (${hitam[hitamStatus]})`;
+        image.src = imageHitam[hitamStatus]; // Ensure image is displayed
+
+        const search = document.querySelector('#search');
+        search.value = '';
     }
 }
 
